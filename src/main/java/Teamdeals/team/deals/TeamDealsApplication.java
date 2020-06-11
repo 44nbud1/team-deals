c84d0d1ddf67d92d80cfe3f15fd70d904459cc8b
Aan Budi Setiawan <setiawanaa@dnid0695l01.local>
2020-06-11 13:23:50 +0700

    Initial commit

---
+++ src/main/java/Teamdeals/team/deals/TeamDealsApplication.java
@@ -0,0 +1,13 @@
+package Teamdeals.team.deals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamDealsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamDealsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Teamdeals/team/deals/TeamDealsApplicationTests.java
@@ -0,0 +1,13 @@
+package Teamdeals.team.deals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamDealsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
